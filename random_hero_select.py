cbf6b27ee9282b72cbf04e38c762ad3e027d2c87
blackhole077 <rajagopal.jeevan@gmail.com>
2020-11-07 01:59:22 -0600

    Got basic GUI working for selecting random hero without filter

--- random_hero_select.py
+++ random_hero_select.py
@@ -12,73 +12,137 @@
 '''
 import os
 import sys
-import numpy as np
-from scipy.special import softmax
-import pandas as pd
+from io import BytesIO
 from json import load
-from typing import List
+from typing import List, Optional, Union
 
-def generate_probability_list(preference_list: List[int]) -> 'numpy.array':
-    """
-        Generate a weighted list based on preferences.
+import numpy as np
+import pandas as pd
+import requests
+from PIL import Image, ImageTk
+from scipy.special import softmax
 
-        Generates a probability distribution based on
-        the softmax function to transform discrete values
-        into a probability distribution that sums to 1.0
 
-        Parameters
-        ----------
-        preference_list: List[int]
-            A list of preference values ranging from 1 to 4.
-            This list must match the number of desired heroes.
+class RandomHeroSelectManager():
+    def __init__(self, preference_location:Optional[str]="config/default_hero_list.json", configuration_location:Optional[str]="config/hero_configuration.json"):
+        self.available_images = [f for f in os.listdir('images/') if os.path.isfile(os.path.join('images/', f))]
+        self.hero_dataframe = self.generate_hero_dataframe(preference_location, configuration_location)
+        self.mask = None
+        # Set the filtered to be equal to the original
+        self.filtered_dataframe = self.hero_dataframe
+        self.hero_probabilities = None
+
+    def generate_mask(self, column_to_check:str, desired_values:Union[List[str],List[int]]):
+        return np.in1d(hero_dataframe[column_to_check].values, desired_values)
+    
+    def generate_combined_mask(self, mask_1, mask_2):
+        return mask_1 & mask_2
+    
+    def callback_primary_stat_mask(self, values):
+        self.mask = self.generate_combined_mask(self.mask, self.generate_mask('primary_stat', values)) 
+        self.generate_filtered_hero_dataframe()
+
+    def callback_position_mask(self, values):
+        self.mask = self.generate_combined_mask(self.mask, self.generate_mask('position', values)) 
+        self.generate_filtered_hero_dataframe()
+
+    def callback_attack_type_mask(self, values):
+        self.mask = self.generate_combined_mask(self.mask, self.generate_mask('attack_type', values)) 
+        self.generate_filtered_hero_dataframe()
+
+    def callback_crowd_control_mask(self, values):
+        self.mask = self.generate_combined_mask(self.mask, self.generate_mask('crowd_control_tags', values))
+        self.generate_filtered_hero_dataframe()
+
+    def generate_filtered_hero_dataframe(self):
+        self.filtered_dataframe = self.hero_dataframe[self.mask]
+        self.hero_probabilities = self.generate_probability_list()
+    
+    def callback_generate_random_hero(self, photo_placeholder:'tkinter.Label', photo_label, hero_text):
+        random_hero = self.filtered_dataframe.sample(weights=self.hero_probabilities).index.values[0]
+        cleaned_hero_text = random_hero.replace('_', ' ').title()
+        img = self.fetch_image(f"{self.hero_dataframe.loc[random_hero, 'image_prefix']}_vert.jpg")
+        photo_placeholder.configure(image=img)
+        photo_placeholder.image = img
+        hero_text.set(cleaned_hero_text)
+        photo_label.update()
         
-        Returns
-        -------
-        weighted_probabilities: np.array(float)
-            A list of real-value probabilities generated via
-            the softmax function. All values will sum to 1.
-    """
+    def fetch_image(self, image_stub: str):
+        if image_stub in self.available_images:
+            return ImageTk.PhotoImage(Image.open(os.path.join('images/', image_stub)))
+        else:
+            url = f"http://cdn.dota2.com/apps/dota2/images/heroes/{image_stub}"
+            response = requests.get(url)
+            if not response.ok:
+                return ImageTk.PhotoImage(Image.open('images/default_vert.jpg'))
+            with open(os.path.join('images/', image_stub), 'wb') as _file:
+                _file.write(response.content)
+            return ImageTk.PhotoImage(Image.open(BytesIO(response.content)))
 
-    return softmax(preference_list)
 
-def generate_hero_dataframe(preference_location: str, configuration_location:str) -> 'pandas.DataFrame':
-    """
-        Generate a dataframe that contains all user-specified data.
+    def generate_probability_list(self) -> 'numpy.array':
+        """
+            Generate a weighted list based on preferences.
 
-        Using the user-defined preference JSON file and the configuration
-        file that contains semi-static information regarding each hero,
-        generate a Dataframe to use while the program is active.
+            Generates a probability distribution based on
+            the softmax function to transform discrete values
+            into a probability distribution that sums to 1.0
 
-        NOTE: Since Pandas is prone to memory leakage, try not to call
-        this function multiple times. If anything, it should only be
-        used once total.
+            Parameters
+            ----------
+            preference_list: List[int]
+                A list of preference values ranging from 1 to 4.
+                This list must match the number of desired heroes.
+            
+            Returns
+            -------
+            weighted_probabilities: np.array(float)
+                A list of real-value probabilities generated via
+                the softmax function. All values will sum to 1.
+        """
 
-        Parameters
-        ----------
-        preference_location: str
-            The name of the JSON file that contains the preferences for
-            each hero in DotA 2.
-        configuration_location: str
-            The name of the JSON file that contains the basic information
-            about each hero in DotA 2, such as primary attribute and attack type.
+        preference_list = self.filtered_dataframe['preference'].values
+        return softmax(preference_list)
 
-        Returns
-        -------
-        hero_dataframe: pandas.DataFrame
-            A DataFrame object that contains all information in both files.
-    """
-    hero_dict = {}
-    with open(configuration_location, 'r') as _config_file:
-        hero_dict = load(_config_file)
-    pref_dict = {}
-    with open(preference_location, 'r') as _preference_file:
-        pref_dict = load(_preference_file)
-    # Manually update one dictionary with values from the other to prevent overwriting
-    for key in hero_dict.keys():
-        hero_dict[key].update(pref_dict.get(key))
-    # Clean out the preference dictionary
-    pref_dict.clear()
-    return pd.DataFrame.from_dict(hero_dict, orient='index')
+    def generate_hero_dataframe(self, preference_location: str, configuration_location:str) -> 'pandas.DataFrame':
+        """
+            Generate a dataframe that contains all user-specified data.
+
+            Using the user-defined preference JSON file and the configuration
+            file that contains semi-static information regarding each hero,
+            generate a Dataframe to use while the program is active.
+
+            NOTE: Since Pandas is prone to memory leakage, try not to call
+            this function multiple times. If anything, it should only be
+            used once total.
+
+            Parameters
+            ----------
+            preference_location: str
+                The name of the JSON file that contains the preferences for
+                each hero in DotA 2.
+            configuration_location: str
+                The name of the JSON file that contains the basic information
+                about each hero in DotA 2, such as primary attribute and attack type.
+
+            Returns
+            -------
+            hero_dataframe: pandas.DataFrame
+                A DataFrame object that contains all information in both files.
+        """
+        
+        hero_dict = {}
+        with open(configuration_location, 'r') as _config_file:
+            hero_dict = load(_config_file)
+        pref_dict = {}
+        with open(preference_location, 'r') as _preference_file:
+            pref_dict = load(_preference_file)
+        # Manually update one dictionary with values from the other to prevent overwriting
+        for key in hero_dict.keys():
+            hero_dict[key].update(pref_dict.get(key))
+        # Clean out the preference dictionary
+        pref_dict.clear()
+        return pd.DataFrame.from_dict(hero_dict, orient='index')
 
 def main():
     '''The main function for rolling (and re-rolling) a random hero
--- random_hero_select_gui.py
+++ random_hero_select_gui.py
@@ -2,35 +2,34 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import requests
 from io import BytesIO
+from random_hero_select import RandomHeroSelectManager as RandomHeroSelectManager
 
 class RandomHeroSelectGui:
     def __init__(self):
-        root = tk.Tk()
-        root.title = "Test GUI"
-        root.geometry = ('600x600')
-        canvas = tk.Canvas(root, bg='red')
-        frame1 = tk.Frame(canvas, bd=1, bg='blue')
-        frame2 = tk.Frame(canvas, bg='purple')
-        button = tk.Button(frame1, text='Next Hero Please!', height=5, width=32)
-        url = "http://cdn.dota2.com/apps/dota2/images/heroes/axe_vert.jpg"
-        response = requests.get(url)
-        img = ImageTk.PhotoImage(Image.open(BytesIO(response.content)))
-        photo_canvas = tk.Canvas(frame1, bg='brown', height=272, width=235)
-        photo_canvas.create_image(235, 272, anchor='se', image=img)
-        label = tk.Label(frame2, text="Right-Hand-Side Info")
-        photo_canvas.pack(side='top', padx=(10, 1), pady=(10,10))
-        label.pack(side='left')
-        button.pack(side='left', padx=(10, 1), pady=(10, 10))
-        frame1.pack(side='left')
-        frame2.pack(side='right')
-        canvas.pack()
-        root.mainloop()
-    
-    def fetch_image(self):
-        # Check if the file exists already locally
+        self.root = tk.Tk()
+        self.root.title("DotA2 Random Hero Selector")
+        self.root.resizable(0,0)
+        self.manager = RandomHeroSelectManager()
+        self.canvas = tk.Canvas(self.root, bg='red')
+        self.frame1 = tk.Frame(self.canvas, bd=1)
+        self.frame2 = tk.Frame(self.canvas, bg='purple')
+        self.current_image = ImageTk.PhotoImage(Image.open('images/default_vert.jpg'))
+        self.photo_placeholder = tk.Label(self.frame1, image=self.current_image)
+        self.photo_placeholder.image = self.current_image
+        self.photo_placeholder.pack(side='top', padx=(10, 10), pady=(10,10))
+        self.hero_text = tk.StringVar()
+        self.photo_label = tk.Label(self.frame1, textvariable=self.hero_text, height=1)
+        self.photo_label.config(font=("Courier", 18))
+        self.photo_label.pack(padx=(10, 10), pady=(10, 10))
+        self.current_image = None
+        self.button = tk.Button(self.frame1, text='Next Hero Please!', height=5, width=32, command = lambda: self.manager.callback_generate_random_hero(self.photo_placeholder, self.photo_label, self.hero_text))
+        self.label = tk.Label(self.frame2, text="Right-Hand-Side Info")
+        self.label.pack(side='left')
+        self.button.pack(side='bottom', padx=(10, 10), pady=(10, 10))
+        self.frame1.pack(side='left')
+        self.frame2.pack(side='right')
+        self.canvas.pack()
+        self.root.mainloop()
 
-        #If not make a request and fetch it from the web and save it
-        pass
 
-    def create_image_holder(self, parent, image_width, image_height):
-        self.photo_canvas = tk.Canvas(parent, height=image_height, width=image_width)
+r = RandomHeroSelectGui()
