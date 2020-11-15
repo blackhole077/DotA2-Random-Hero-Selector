b40bd1303e6d97fa93ac68f5b9db552dcd2a63ed
blackhole077 <rajagopal.jeevan@gmail.com>
2020-11-14 22:49:10 -0600

    Added docstrings to most functions and classes

--- random_hero_select.py
+++ random_hero_select.py
@@ -1,4 +1,4 @@
-'''
+"""
     What Hero Do I Want? A Random-Button Killer
     Written by Jeevan Rajagopal
     01/13/20
@@ -9,7 +9,8 @@
     heroes. This allows the user to have the
     fun of random selection, without making
     their allies suffer the consequences.
-'''
+"""
+
 import os
 import sys
 from io import BytesIO
@@ -23,7 +24,48 @@ from PIL import Image, ImageTk
 from scipy.special import softmax
 
 
-class RandomHeroSelectManager():
+class RandomHeroSelectManager:
+    """
+        Construct a logic layer that handles callback functions
+        and user-defined information.
+
+        This class is used to convert user-defined data into
+        Pandas DataFrames, which are then used for rolling
+        random heroes that fit user-defined filters and parameters.
+        
+        Attributes
+        ----------
+        available_images: List[str]
+            A list of file paths corresponding to hero portraits.
+        hero_dataframe: pandas.DataFrame
+            The data structure used to hold the hero information,
+            along with filter-specific information.
+        mask: numpy.array
+            A boolean array that masks out certain heroes in the
+            hero_dataframe data structure. Multiple filters and
+            masks are combined by LOGCIAL AND into a single mask.
+        filtered_dataframe: pandas.DataFrame
+            A DataFrame that represents a filtered subsection of
+            the main hero_dataframe. This is equivalent to the
+            main DataFrame if no masks are present.
+        hero_probabilities: numpy.array
+            A 1D Numpy array that represents the likelihood of
+            a given hero being selected. Due to the use of the
+            Softmax function, all probabilities will sum to 1.
+
+        Parameters
+        ----------
+        preference_location: Optional[str]
+            The file path location of the JSON file to use
+            for hero preferences. The values are scaled from
+            0 to 4, with higher numbers indicating a higher
+            desire to play.
+        configuration_location: Optional[str]
+            The file path location of the JSON file that
+            contains the filter-specific information, along
+            with other static pieces of information.
+    """
+
     def __init__(self, preference_location:Optional[str]="config/default_hero_list.json", configuration_location:Optional[str]="config/hero_configuration.json"):
         self.available_images = [f for f in os.listdir('images/') if os.path.isfile(os.path.join('images/', f))]
         self.hero_dataframe = self.generate_hero_dataframe(preference_location, configuration_location)
@@ -33,12 +75,85 @@ class RandomHeroSelectManager():
         self.hero_probabilities = None
 
     def generate_mask(self, column_to_check:str, desired_values:Union[List[str],List[int]]):
+        """
+            Generate a filter to apply.
+
+            Generates a boolean array that checks, for a given column, if the value
+            contained in said column is present in the list of desired values.
+
+            NOTE: The shape of the mask should be equal to the number of heroes
+            present in the data structure. Basically masks.shape = (num_heroes, )
+
+            Parameters
+            ----------
+            column_to_check: str
+                The name of the column to use for reference
+            desired_values: Union[List[str], List[int]]
+                A list containing either string values or integer 
+                values that represent a 'whitelist' of accepted values
+                for the mask.
+
+            Returns
+            -------
+            masks: numpy.array
+                A boolean array that masks out certain heroes in the
+                hero_dataframe data structure. Multiple filters and
+                masks are combined by LOGCIAL AND into a single mask.
+        """
+
         return np.in1d(hero_dataframe[column_to_check].values, desired_values)
     
     def generate_combined_mask(self, mask_1, mask_2):
+        """
+            Generates a combination of multiple masks.
+
+            Given two boolean masks, this function
+            applies a LOGICAL AND to combine them into
+            one mask.
+
+            NOTE: The shape of the mask should be equal to the number of heroes
+            present in the data structure. Basically masks.shape = (num_heroes, )
+
+            Parameters
+            ----------
+            mask_1: numpy.array
+                A boolean array that masks out certain heroes in the
+                hero_dataframe data structure. Multiple filters and
+                masks are combined by LOGCIAL AND into a single mask.
+            mask_2: numpy.array
+                A boolean array that masks out certain heroes in the
+                hero_dataframe data structure. Multiple filters and
+                masks are combined by LOGCIAL AND into a single mask.
+
+            Returns
+            -------
+            combined_mask: numpy.array
+                A boolean array that masks out certain heroes in the
+                hero_dataframe data structure. Multiple filters and
+                masks are combined by LOGCIAL AND into a single mask.
+        """
+
         return mask_1 & mask_2
     
     def callback_primary_stat_mask(self, values):
+        """
+            A callback function to filter the heroes
+            based on their primary attribute.
+
+            This function creates and applies a filter
+            that results in a subset of heroes whose
+            primary attribute matches the desired values.
+
+            Parameters
+            ----------
+            values: Unknown.
+                Writeme.
+
+            Returns
+            -------
+            None.
+        """
+
         self.mask = self.generate_combined_mask(self.mask, self.generate_mask('primary_stat', values)) 
         self.generate_filtered_hero_dataframe()
 
@@ -55,10 +170,48 @@ class RandomHeroSelectManager():
         self.generate_filtered_hero_dataframe()
 
     def generate_filtered_hero_dataframe(self):
+        """
+            Apply existing filters to create the filtered DataFrame and create the probabilities.
+
+            This function applies any existing masks onto the main DataFrame,
+            after which the new probabilities are generated.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            None.
+        """
+
         self.filtered_dataframe = self.hero_dataframe[self.mask]
         self.hero_probabilities = self.generate_probability_list()
     
-    def callback_generate_random_hero(self, photo_placeholder:'tkinter.Label', photo_label, hero_text):
+    def callback_generate_random_hero(self, photo_placeholder:'tkinter.Label', photo_label:'tkinter.Label', hero_text:'tkinter.StringVar') -> None:
+        """
+            A callback function to randomly select a hero and display the corresponding image.
+
+            This function selects a random hero from the filtered list of available heroes,
+            searches for the corresponding image and then displays the image in the HeroPortrait.
+
+            Parameters
+            ----------
+            photo_placeholder: tkinter.Label
+                A label that serves to house the image.
+                NOTE: Originally a Canvas was used for this purpose,
+                but ultimately Label proved more understandable for this project.
+            photo_label: tkinter.Label
+                A label that houses the name of the hero the image belongs to.
+            hero_text: tkinter.StringVar
+                A variable that holds the actual mutable text data corresponding
+                to the randomly selected hero.
+
+            Returns
+            -------
+            None.
+        """
+        
         random_hero = self.filtered_dataframe.sample(weights=self.hero_probabilities).index.values[0]
         cleaned_hero_text = random_hero.replace('_', ' ').title()
         img = self.fetch_image(f"{self.hero_dataframe.loc[random_hero, 'image_prefix']}_vert.jpg")
@@ -67,7 +220,27 @@ class RandomHeroSelectManager():
         hero_text.set(cleaned_hero_text)
         photo_label.update()
         
-    def fetch_image(self, image_stub: str):
+    def fetch_image(self, image_stub: str) -> 'ImageTk.PhotoImage':
+        """
+            Fetch the image corresponding to the randomly selected hero.
+
+            This function attempts to retrieve a hero portrait locally.
+            If the image is not available locally, it is fetched from the
+            DotA2 website directly and saved in the local `images` folder.
+
+            Parameters
+            ----------
+            image_stub: str
+                A string that uniquely identifies the hero that was selected.
+                It is also used in the URL, if the local file is not present.
+            
+            Returns
+            -------
+            hero_portrait: ImageTk.PhotoImage
+                The image that is currently displayed. A default image is loaded
+                upon initialization.
+        """
+
         if image_stub in self.available_images:
             return ImageTk.PhotoImage(Image.open(os.path.join('images/', image_stub)))
         else:
@@ -145,9 +318,12 @@ class RandomHeroSelectManager():
         return pd.DataFrame.from_dict(hero_dict, orient='index')
 
 def main():
-    '''The main function for rolling (and re-rolling) a random hero
-       according to your weighted preferences.
-    '''
+    """
+        The main function for rolling (and re-rolling) a random hero
+        according to your weighted preferences.
+        This is essentially a debug function, and is not used in the application.
+    """
+
     hero_dataframe = generate_hero_dataframe('config/default_hero_list.json', 'config/hero_configuration.json')
     print(hero_dataframe)
     mask = np.in1d(hero_dataframe['primary_stat'].values, ['strength', 'intelligence'])
@@ -158,10 +334,6 @@ def main():
     weighted_probabilities = generate_probability_list(df_slice['preference'].values)
     for _ in range(5):
         print(df_slice.sample(weights=weighted_probabilities).index.values)
-    # TODO: Write call for converting JSON files into DataFrame
-    # TODO: Test that weighted random via softmax works as intended
-    # TODO: Write this main function as a callback on start for tkinter
-    pass
 
 if __name__ == "__main__":
     main()
--- random_hero_select_gui.py
+++ random_hero_select_gui.py
@@ -1,12 +1,48 @@
 import tkinter as tk
-from PIL import ImageTk, Image
-import requests
-from io import BytesIO
-from random_hero_select import RandomHeroSelectManager as RandomHeroSelectManager
 import tkinter.ttk as ttk
+from io import BytesIO
+from typing import Union
+
+import requests
+from PIL import Image, ImageTk
+
+from random_hero_select import \
+    RandomHeroSelectManager as RandomHeroSelectManager
+
 
 class HeroPortrait:
-    def __init__(self, parent):
+    """
+        The subcomponent that contains the hero portraits for the DotA2 Random Hero Selector.
+
+        This GUI sub-component serves to house the photo container, as well as
+        a text box that indicates the name of the hero the image belongs to.
+
+        Attributes
+        ----------
+        current_image: ImageTk.PhotoImage
+            The image that is currently displayed. A default image is loaded
+            upon initialization.
+        photo_placeholder: tkinter.Label
+            A label that serves to house the image.
+            NOTE: Originally a Canvas was used for this purpose,
+            but ultimately Label proved more understandable for this project.
+        photo_label: tkinter.Label
+            A label that houses the name of the hero the image belongs to.
+        hero_text: tkinter.StringVar
+            A variable that holds the actual mutable text data corresponding
+            to the randomly selected hero.
+
+        Parameters
+        ----------
+        parent: tkinter.Frame
+            The Frame (or Frame) that will contain this subcomponent
+        
+        Returns
+        -------
+        None.
+    """
+
+    def __init__(self, parent: Union['tkinter.Frame', 'tkinter.Canvas']) -> None:
         self.current_image = ImageTk.PhotoImage(Image.open('images/default_vert.jpg'))
         self.photo_placeholder = tk.Label(parent, image=self.current_image)
         self.photo_placeholder.image = self.current_image
@@ -18,7 +54,7 @@ class HeroPortrait:
         self.photo_label.pack(padx=(10, 10), pady=(10, 10))
 
 class FilterGui:
-    def __init__(self, parent):
+    def __init__(self, parent: Union['tkinter.Frame', 'tkinter.Canvas']) -> None:
         self.filter_parent = tk.ttk.Labelframe(parent, text='Filters')
         self.upper_filter = tk.Frame(self.filter_parent, bg='grey')
         self.upper_filter_label = tk.Label(self.upper_filter, text="Upper Filter Section")
@@ -41,9 +77,37 @@ class RandomHeroSelectGui:
         
         NOTE: In future updates, this will also house the FilterGUI
         and its children components.
+
+        Attributes
+        ----------
+        root: tkinter.Tk
+            The root window that will contain all
+            GUI components.
+        manager: random_hero_select.RandomHeroSelectManager
+            The custom class that serves as the logic
+            layer for randomly selecting heroes.
+            It also contains all callback functions.
+        main_canvas: tkinter.Canvas
+            The main canvas that houses all components.
+        hero_portrait: HeroPortrait
+            A custom component that contains the hero portrait.
+            For more information, look at the class documentation
+            for HeroPortrait.
+        random_button: tkinter.Button
+            A component used to activate the random hero selection.
+
+
+        Parameters
+        ----------
+        None.
+
+        Returns
+        -------
+        None.
+
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         # Create the root window
         self.root = tk.Tk()
         # Set the title
@@ -53,23 +117,24 @@ class RandomHeroSelectGui:
         # Load in the random selection logic
         self.manager = RandomHeroSelectManager()
         # Create the main canvas
-        self.canvas = tk.Canvas(self.root, bg='black')
+        self.main_canvas = tk.Canvas(self.root, bg='black')
         # Pack the main canvas
-        self.canvas.pack()
+        self.main_canvas.pack()
         # Generate the hero portrait component and attach it
-        self.hero_portrait_frame = tk.Frame(self.canvas)
+        self.hero_portrait_frame = tk.Frame(self.main_canvas)
         self.hero_portrait = HeroPortrait(self.hero_portrait_frame)
         # Pack the HeroPortrait component into the main canvas
         self.hero_portrait_frame.pack(side='left')
         # Create the Random button and attach the approprate callback parameters from the HeroPortrait component
-        self.button = tk.Button(self.hero_portrait_frame, text='Next Hero Please!', height=5, width=32, command = lambda: self.manager.callback_generate_random_hero(self.hero_portrait.photo_placeholder, self.hero_portrait.photo_label, self.hero_portrait.hero_text))
+        self.random_button = tk.Button(self.hero_portrait_frame, text='Next Hero Please!', height=5, width=32, command = lambda: self.manager.callback_generate_random_hero(self.hero_portrait.photo_placeholder, self.hero_portrait.photo_label, self.hero_portrait.hero_text))
         # Make the button look raised
-        self.button.config(relief=tk.RAISED)
+        self.random_button.config(relief=tk.RAISED)
         # Pack the button towards the bottom of the hero_portait frame (bottom-left) with padding.
-        self.button.pack(side='bottom', padx=(10, 10), pady=(10, 10))
-        self.filter_frame = FilterGui(self.canvas)
+        self.random_button.pack(side='bottom', padx=(10, 10), pady=(10, 10))
+        self.filter_frame = FilterGui(self.main_canvas)
         # Run the root window GUI
         self.root.mainloop()
 
 
-r = RandomHeroSelectGui()
+if __name__ == "__main__":
+    r = RandomHeroSelectGui()
