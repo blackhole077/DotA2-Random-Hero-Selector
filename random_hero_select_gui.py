5c43a8af231f0535edc97e2860ff13eb88c403a1
blackhole077 <rajagopal.jeevan@gmail.com>
2020-11-17 12:38:14 -0600

    Documented all GUI components and added type annotation

--- random_hero_select_gui.py
+++ random_hero_select_gui.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import tkinter.ttk as ttk
 from io import BytesIO
-from typing import Union, Dict, List
+from typing import Dict, List, Union, Iterable
 
 import requests
 from PIL import Image, ImageTk
@@ -9,32 +9,102 @@ from PIL import Image, ImageTk
 from random_hero_select import \
     RandomHeroSelectManager as RandomHeroSelectManager
 
+
 class Checkbar(tk.Frame):
-    def __init__(self, parent=None, picks=None, side="left", anchor="w"):
+    """
+        Create a row or grid of checkboxes.
+
+        Attributes
+        ----------
+        vars: List[tkinter.Checkbutton]
+            The list of checkboxes.
+        labels: List[str]
+            The names attached to the checkboxes, as well as the
+            potential value of a given checkbox if fetch_selected_labels.
+
+        Parameters
+        ----------
+        parent: 'tkinter.Frame'
+            The parent container or component the Checkbar is attached to.
+        labels: List[str]
+            The list of labels for the checkboxes.
+        max_per_row: int
+            The maximum number of checkboxes allowed before a new row is created.
+            Default behavior will create only one row if the number of checkboxes
+            (determined by the length of labels) is less than or equal to this value.
+        
+        Returns
+        -------
+        None.
+    """
+
+    def __init__(self, parent:'tkinter.Frame', labels:List[str], max_per_row: int=5) -> None:
         tk.Frame.__init__(self, parent)
         self.vars = []
-        self.labels = picks
+        self.labels = labels
         row = 0
         column = 0
-        for index, pick in enumerate(picks):
-            if index % 7 == 0:
-                row += 1
-                column = 0
+        for index, label in enumerate(labels):
+            if len(labels) > max_per_row:
+                if index % max_per_row == 0:
+                    row += 1
+                    column = 0
             var = tk.IntVar()
-            chk = tk.Checkbutton(self, text=pick.capitalize(), variable=var)
-            chk.grid(row=row, column=column)
+            chk = tk.Checkbutton(self, text="{:20s}".format(label.replace("_", " ").title()), variable=var)
+            chk.grid(row=row, column=column, sticky='w')
             column += 1
             self.vars.append(var)
     
-    def state(self):
+    def state(self) -> Iterable[int]:
+        """
+            Fetch the state of all checkboxes in the Checkbar.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            checkbox_states: Iterable[int]
+                A map object that contains integer values between [0,1] that
+                indicate whether the checkbox is toggled on or not.
+        """
+
         return map((lambda var: var.get()), self.vars)
     
-    def clear(self):
+    def clear(self) -> None:
+        """
+            Clear all checkboxes in the Checkbar.
+
+            Clears all checkboxes by setting their value to 0.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            None.
+        """
+
         for var in self.vars:
             var.set(0)
 
-    def fetch_selected_labels(self):
-        return  [value for (value, filter) in zip(self.labels, [var.get() for var in self.vars]) if filter]
+    def fetch_selected_labels(self) -> List[str]:
+        """
+            Fetch the labels of the checkboxes that were toggled in the Checkbar.
+
+            Parameters
+            ----------
+            None.
+
+            Returns
+            -------
+            selected_labels: List[str]
+                A list of all labels whose checkboxes were toggled.
+        """
+
+        return [label_value for (label_value, checkbox_value) in zip(self.labels, list(self.state())) if checkbox_value]
 
 class HeroPortrait:
     """
@@ -79,34 +149,103 @@ class HeroPortrait:
         self.photo_label.config(font=("Courier", 18))
         self.photo_label.pack(padx=(10, 10), pady=(10, 10))
 
-class FilterGui:
+class FilterGUI:
+    """
+        The subcomponent used to generate checkbox-based filters for the DotA2 Random Hero Selector.
+
+        Attributes
+        ----------
+        filter_parent: tkinter.Frame
+            A basic container to house the checkboxes.
+        filter_parent_label: tkinter.Label
+            The label that contains the name of the filter.
+        checkboxes: Checkbar
+            The checkboxes used to control which filters, if any, are to be applied.
+
+        Parameters
+        ----------
+        parent: Union['tkinter.Frame', 'tkinter.Canvas']
+            The parent container or component the FilterPanel is attached to.
+        filter_name: str
+            The name of the filter. Should correspond to some value present
+            in the hero_configuration JSON file.
+        filter_values: List[str]
+            The list of possible values that are allowed to be toggled.
+        
+        Returns
+        -------
+        None.
+    """
+
     def __init__(self, parent: Union['tkinter.Frame', 'tkinter.Canvas'], filter_name: str, filter_values: List[str]) -> None:
-        self.upper_filter = tk.Frame(parent, bg='grey')
+        self.filter_parent = tk.Frame(parent, bg='grey')
         name = filter_name.replace('_', ' ').title()
-        self.upper_filter_label = tk.Label(self.upper_filter, text=f"Filter by {name}")
-        self.upper_filter.pack(side='top')
-        self.upper_filter_label.pack(side='top')
+        self.filter_parent_label = tk.Label(self.filter_parent, text=f"Filter by {name}")
+        self.filter_parent.pack(side='top')
+        self.filter_parent_label.pack(side='top')
         self.checkboxes = Checkbar(parent, filter_values)
-        self.checkboxes.pack()
-        s = tk.ttk.Separator(self.upper_filter, orient=tk.HORIZONTAL)
+        self.checkboxes.pack(padx=(10, 10), pady=(10, 10))
+        s = tk.ttk.Separator(self.filter_parent, orient=tk.HORIZONTAL)
         s.pack()
 
 class FilterPanel:
-    def __init__(self, parent: Union['tkinter.Frame', 'tkinter.Canvas'], filter_dict: Dict[str, List[str]], logic_layer: RandomHeroSelectManager):
+    """
+        The secondary GUI component for the DotA2 Random Hero Selector.
+
+        This GUI component houses multiple subcomponents, along with
+        two buttons that control the application/removal of filters.
+
+        Attributes
+        ----------
+        filter_parent: tkinter.ttk.Labelframe
+            A container that houses all filters.
+        filters: Dict[str, Checkbox]
+            A dictionary structure that maps the names
+            of the filters along with their respective checkboxes.
+            This is passed to the manager/logic_layer for filtering the hero pool.
+        filter_button_frame: tkinter.Frame
+            A basic container to separate the buttons from the checkboxes.
+        filter_application_button: tkinter.Button
+            The button used to apply all filters selected.
+            If no filters are selected, the behavior is identical to all filters being selected.
+        filter_clear_button: tkinter.Button
+            The button used to clear all filters and reset the hero pool to its original form.
+        
+        Parameters
+        ----------
+        parent: Union['tkinter.Frame', 'tkinter.Canvas']
+            The parent container or component the FilterPanel is attached to.
+        filter_dict: Dict[str, List[str]]
+            The data structure used to populate the text and values for the filters
+            inside of the FilterPanel component.
+        logic_layer: RandomHeroSelectManager
+            The custom class that serves as the logic layer for randomly selecting heroes.
+            It also contains all callback functions.
+
+        Returns
+        -------
+        None.
+    """
+
+    def __init__(self, parent: Union['tkinter.Frame', 'tkinter.Canvas'], filter_dict: Dict[str, List[str]], logic_layer: RandomHeroSelectManager) -> None:
         self.filter_parent = tk.ttk.Labelframe(parent, text='Filters')
         self.filters = {}
+        # Generate FilterGUIs for each filter specified
         for key, value in filter_dict.items():
-            filter = FilterGui(self.filter_parent, key, value)
+            filter = FilterGUI(self.filter_parent, key, value)
+            # Update the filters dictionary structure
             self.filters[key] = filter.checkboxes
+        # Set up the button subcomponent
         self.filter_button_frame = tk.Frame(parent)
         self.filter_application_button = tk.Button(self.filter_button_frame, text='Apply Filters', width=10, height=1, fg='blue', command= lambda: logic_layer.callback_generate_masks(self.filters))
         self.filter_clear_button = tk.Button(self.filter_button_frame, text='Clear Filters', width=10, height=1, fg='blue', command= lambda: logic_layer.callback_clear_masks(self.filters))
+        # Pack all components
         self.filter_application_button.pack(side='left')
         self.filter_clear_button.pack(side='right')
         self.filter_parent.pack(side='top', padx=(10, 10), pady=(10, 10))
         self.filter_button_frame.pack(side='bottom', padx=(10, 10), pady=(10, 10))
 
-class RandomHeroSelectGui:
+class RandomHeroSelectGUI:
     """
         The main GUI component for the DotA2 Random Hero Selector.
 
@@ -155,7 +294,7 @@ class RandomHeroSelectGui:
         # Load in the random selection logic
         self.manager = RandomHeroSelectManager()
         # Create the main canvas
-        self.main_canvas = tk.Canvas(self.root, bg='black')
+        self.main_canvas = tk.Canvas(self.root)
         # Pack the main canvas
         self.main_canvas.pack()
         # Generate the hero portrait component and attach it
@@ -163,7 +302,7 @@ class RandomHeroSelectGui:
         self.hero_portrait = HeroPortrait(self.hero_portrait_frame)
         # Pack the HeroPortrait component into the main canvas
         self.hero_portrait_frame.pack(side='left')
-        # Create the Random button and attach the approprate callback parameters from the HeroPortrait component
+        # Create the Random button and attach the appropriate callback parameters from the HeroPortrait component
         self.random_button = tk.Button(self.hero_portrait_frame, text='Next Hero Please!', height=5, width=32, command = lambda: self.manager.callback_generate_random_hero(self.hero_portrait.photo_placeholder, self.hero_portrait.photo_label, self.hero_portrait.hero_text))
         # Make the button look raised
         self.random_button.config(relief=tk.RAISED)
@@ -175,4 +314,4 @@ class RandomHeroSelectGui:
 
 
 if __name__ == "__main__":
-    r = RandomHeroSelectGui()
+    r = RandomHeroSelectGUI()
